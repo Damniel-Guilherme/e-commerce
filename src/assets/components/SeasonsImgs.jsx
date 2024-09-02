4918b46fa3f8c030c3b5d537f1b0805a510e0d57
DAMNIEL <114008220+Damniel-Guilherme@users.noreply.github.com>
2024-09-01 23:08:12 -0300

    att

--- src/assets/components/SeasonsImgs.jsx
+++ src/assets/components/SeasonsImgs.jsx
@@ -1,16 +1,39 @@
 import styled from 'styled-components';
 
-const Testee = styled.div`
-    font-size: 30px;
-    color: red;
+const ContainerCollections = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+    width: 100%;
+    max-width: 1920px;
+    height: 900px;
+    background-color: red;
+
+    padding: 0 30px;
+`;
+
+const ContainerCollectionsEstation = styled.div`
+    width: 30%;
+    height: 850px;
+    background-color: blue;
+`;
+
+const ContainerCollectionsEstation2 = styled.div`
+    width: 70%;
+    height: 850px;
+    background-color: blue;
 `;
 
 
 const SeasonsImgs = () => {
     return (
         <>
-            <h2>TEST</h2>
-           <Testee>test2</Testee>
+           <ContainerCollections>
+            <ContainerCollectionsEstation></ContainerCollectionsEstation>
+            <ContainerCollectionsEstation2></ContainerCollectionsEstation2>
+           </ContainerCollections>
         </>
 
     );
